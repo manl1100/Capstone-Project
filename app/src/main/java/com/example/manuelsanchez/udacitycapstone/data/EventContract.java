b6126179e82568ba1a1f33daf854be4835a23ec7
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-04-10 00:31:35 -0500

    Event Contract

---
+++ app/src/main/java/com/example/manuelsanchez/udacitycapstone/data/EventContract.java
@@ -0,0 +1,30 @@
+package com.example.manuelsanchez.udacitycapstone.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by manuelsanchez on 4/10/16
+ */
+public class EventContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.udacitycapstone.app";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_EVENT = "event";
+
+
+    public static final class EventEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_EVENT).build();
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY +"/" + PATH_EVENT;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY +"/" + PATH_EVENT;
+
+        public static final String TABLE_NAME =  "event";
+        public static final String COLUMN_VENUE = "venue";
+        public static final String COLUMN_PERFORMER = "performer";
+        public static final String COLUMN_COORD_LATITUDE = "coord_lat";
+        public static final String COLUMN_COORD_LONGITUDE = "coord_long";
+        public static final String COLUMN_DATE = "event_date";
+    }
+}
