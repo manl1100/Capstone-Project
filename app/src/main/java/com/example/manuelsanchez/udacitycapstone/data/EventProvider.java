24abdeb9cfec8d41df9108754b802e8129fad9c3
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-04-14 20:47:49 -0500

    Simple query in event provider

--- app/src/main/java/com/example/manuelsanchez/udacitycapstone/data/EventProvider.java
+++ app/src/main/java/com/example/manuelsanchez/udacitycapstone/data/EventProvider.java
@@ -3,22 +3,47 @@ package com.example.manuelsanchez.udacitycapstone.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
 public class EventProvider extends ContentProvider {
 
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private static final int EVENT = 100;
+
+    private EventDbHelper mOpenHelper;
 
     @Override
     public boolean onCreate() {
+        mOpenHelper = new EventDbHelper(getContext());
         return false;
     }
 
     @Nullable
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        Cursor returnCursor;
+        switch (sUriMatcher.match(uri)) {
+            case EVENT: {
+                returnCursor = mOpenHelper.getReadableDatabase().query(
+                        EventContract.EventEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        returnCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return returnCursor;
     }
 
     @Nullable
@@ -41,5 +66,14 @@ public class EventProvider extends ContentProvider {
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         return 0;
+    }
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = EventContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, EventContract.PATH_EVENT, EVENT);
+
+        return matcher;
     }
 }
