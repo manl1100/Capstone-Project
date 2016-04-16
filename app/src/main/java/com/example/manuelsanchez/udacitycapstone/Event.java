7291bc4b31e754d3c7f7b7b2bed19b9b8b06b69d
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-04-16 01:29:27 -0500

    Parcelable event

--- app/src/main/java/com/example/manuelsanchez/udacitycapstone/Event.java
+++ app/src/main/java/com/example/manuelsanchez/udacitycapstone/Event.java
@@ -1,14 +1,40 @@
 package com.example.manuelsanchez.udacitycapstone;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class Event {
+public class Event implements Parcelable {
 
     private String eventDate;
     private String venueName;
     private double latitude;
     private double longitute;
     private List<Artist> artists;
+
+    public Event() {
+
+    }
+
+    protected Event(Parcel in) {
+        eventDate = in.readString();
+        venueName = in.readString();
+        latitude = in.readDouble();
+        longitute = in.readDouble();
+    }
+
+    public static final Creator<Event> CREATOR = new Creator<Event>() {
+        @Override
+        public Event createFromParcel(Parcel in) {
+            return new Event(in);
+        }
+
+        @Override
+        public Event[] newArray(int size) {
+            return new Event[size];
+        }
+    };
 
     public String getEventDate() {
         return eventDate;
@@ -52,6 +78,19 @@ public class Event {
 
     public String getHeadLinerName() {
         return artists.get(0).artistName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(eventDate);
+        dest.writeString(venueName);
+        dest.writeDouble(latitude);
+        dest.writeDouble(longitute);
     }
 
     public static class Builder {
