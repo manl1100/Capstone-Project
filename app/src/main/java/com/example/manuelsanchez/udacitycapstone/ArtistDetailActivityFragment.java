f29fe05e47aaece2701bc223615aff4acecbad6e
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-04-28 23:26:10 -0500

    Implemented tour date screen for selected artist

--- app/src/main/java/com/example/manuelsanchez/udacitycapstone/ArtistDetailActivityFragment.java
+++ app/src/main/java/com/example/manuelsanchez/udacitycapstone/ArtistDetailActivityFragment.java
@@ -1,20 +1,24 @@
 package com.example.manuelsanchez.udacitycapstone;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ArtistDetailActivityFragment extends Fragment {
 
     public static final String ARG_ITEM_ID = "item_id";
+    private TourDateRecyclerViewAdapter mTourDateRecyclerViewAdapter;
     private Artist mArtist;
 
     public ArtistDetailActivityFragment() {
@@ -36,8 +40,65 @@ public class ArtistDetailActivityFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_artist_detail, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_artist_detail, container, false);
+
+        if (mArtist != null) {
+            ((TextView) view.findViewById(R.id.performing_artist)).setText(mArtist.artistName);
+            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.tour_dates);
+            mTourDateRecyclerViewAdapter = new TourDateRecyclerViewAdapter();
+            recyclerView.setAdapter(mTourDateRecyclerViewAdapter);
+            new EventDetailAsyncTask(getContext(), mTourDateRecyclerViewAdapter).execute(mArtist.getEventfulArtistId());
+        }
+
+        return view;
+    }
+
+    public class TourDateRecyclerViewAdapter extends RecyclerView.Adapter<TourDateRecyclerViewAdapter.ViewHolder> {
+
+        private List<Event> mValues;
+
+        public TourDateRecyclerViewAdapter() {
+            this.mValues = new ArrayList<>();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tour_date_list_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.performanceDate.setText(DateUtil.getFormattedDateString(mValues.get(position).getEventDate()));
+            holder.venue.setText(mValues.get(position).getVenueName());
+            holder.location.setText(mValues.get(position).getVenueAddress());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public void setEventDates(List<Event> events) {
+            mValues = events;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView performanceDate;
+            public final TextView venue;
+            public final TextView location;
+            public Event mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                performanceDate = (TextView) view.findViewById(R.id.performance_date);
+                venue = (TextView) view.findViewById(R.id.venue_name);
+                location = (TextView) view.findViewById(R.id.venue_location);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/manuelsanchez/udacitycapstone/EventDetailAsyncTask.java
+++ app/src/main/java/com/example/manuelsanchez/udacitycapstone/EventDetailAsyncTask.java
@@ -1,8 +1,139 @@
 package com.example.manuelsanchez.udacitycapstone;
 
 
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
 
-public class EventDetailAsyncTask {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventDetailAsyncTask extends AsyncTask<String, Void, List<Event>> {
+
+    private final String LOG_TAG = EventDetailAsyncTask.class.getSimpleName();
+
+    private ArtistDetailActivityFragment.TourDateRecyclerViewAdapter mTourDateRecyclerViewAdapter;
+    private Context mContext;
+
+    public EventDetailAsyncTask(Context context, ArtistDetailActivityFragment.TourDateRecyclerViewAdapter tourDateRecyclerViewAdapter) {
+        mContext = context;
+        mTourDateRecyclerViewAdapter = tourDateRecyclerViewAdapter;
+    }
+
+    @Override
+    protected List<Event> doInBackground(String... params) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String concertString = null;
+
+        try {
+            String urlBase = "http://api.eventful.com/json/performers/events/list?";
+            String apiKeyParam = "app_key";
+            String apiKey = mContext.getString(R.string.eventful_api_key);
+            String performerIdParam = "id";
+
+
+            Uri builtUri = Uri.parse(urlBase).buildUpon()
+                    .appendQueryParameter(apiKeyParam, apiKey)
+                    .appendQueryParameter(performerIdParam, params[0])
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            concertString = buffer.toString();
+            Log.i(LOG_TAG, "JsonString: " + concertString);
+
+
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Error", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
+
+        return parseTourString(concertString);
+
+    }
+
+    private List<Event> parseTourString(String response) {
+        List<Event> events = new ArrayList<Event>();
+
+        final String JSON_ARRAY_EVENT = "event";
+        final String JSON_STRING_COUNTRY = "country";
+        final String JSON_STRING_REGION = "region";
+        final String JSON_STRING_CITY = "city";
+        final String JSON_STRING_EVENT_ID = "id";
+        final String JSON_STRING_TIME = "start_time";
+
+        try {
+            JSONObject responseObject = new JSONObject(response);
+            JSONArray eventArray = responseObject.getJSONArray(JSON_ARRAY_EVENT);
+
+            for (int i = 0; i < eventArray.length(); i++) {
+                JSONObject eventItem = eventArray.getJSONObject(i);
+
+                String country = eventItem.getString(JSON_STRING_COUNTRY);
+                String region = eventItem.getString(JSON_STRING_REGION);
+                String city = eventItem.getString(JSON_STRING_CITY);
+                String eventId = eventItem.getString(JSON_STRING_EVENT_ID);
+                String eventDate = eventItem.getString(JSON_STRING_TIME);
+                Event event = new Event.Builder()
+                        .country(country)
+                        .city(city)
+                        .eventDate(eventDate)
+                        .eventId(eventId)
+                        .region(region)
+                        .build();
+                events.add(event);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return events;
+    }
+
+    @Override
+    protected void onPostExecute(List<Event> events) {
+        mTourDateRecyclerViewAdapter.setEventDates(events);
+        mTourDateRecyclerViewAdapter.notifyDataSetChanged();
+    }
 
 }
