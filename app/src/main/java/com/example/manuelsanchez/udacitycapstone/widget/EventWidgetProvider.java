b6d953efe36af016b5229fb95d24656585fc335b
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-06-05 12:49:51 -0500

    Initial event widget provider

---
+++ app/src/main/java/com/example/manuelsanchez/udacitycapstone/widget/EventWidgetProvider.java
@@ -0,0 +1,20 @@
+package com.example.manuelsanchez.udacitycapstone.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class EventWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
